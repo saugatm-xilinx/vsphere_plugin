03158dde1031ca1e985707a492294eaaaba608b7
kpraveen <unknown>
2018-01-22 16:41:39 +0530

    Changed after reviewed by Belal
    hg-changeset: 423ee01e60d0352b238ca0be05c46bab41c1599e
    hg-branch: new_framework

--- firmware-packaging-tool/src/com/solarflare/firmware/Runner.java
+++ firmware-packaging-tool/src/com/solarflare/firmware/Runner.java
@@ -1,43 +1,80 @@
 package com.solarflare.firmware;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 import com.solarflare.firmware.util.MetadataGeneratorHelper;
 
-public class Runner {
+public class Runner
+{
+    static int basePathLastIndex;
 
-	static String SEPARATOR = File.separator;
-	static final String RELATIVE_PATH = "installer" + SEPARATOR + "dist"
-			+ SEPARATOR + "Tomcat_Server" + SEPARATOR + "webapps" + SEPARATOR
-			+ "firmware" + SEPARATOR;
-	static int basePathLastIndex;
-
-	public static void main(String args[]){
-		try {
-			String path = "";
-			File file = null;
-			int lastIndex = 0;
-			// First check in argument
-			if (args.length != 0) {
-				path = args[0];
-				path = path + File.separator;
-				lastIndex = path.lastIndexOf(File.separator);
-				file = new File(path);
-			} 
-			else if (System.getenv("FIRMWARE_PATH") != null)// Second check on environment variable
-			{
-				path = System.getenv("FIRMWARE_PATH");
-				path = path + File.separator;
-				lastIndex = path.lastIndexOf(File.separator);
-				file = new File(path);
-			} else {
-
-				throw new RuntimeException("binary file path not found !!!");
-			}
-			MetadataGeneratorHelper.generateMetadataJSON(file, lastIndex);
-			System.out.println("Firmware metadata file generated successfuuly in path :" + file.getAbsolutePath());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String args[])
+    {
+        try
+        {
+            String path = "";
+            File file = null;
+            int lastIndex = 0;
+            // First check in argument
+            if (args.length != 0)
+            {
+                path = args[0];
+                file = new File(path);
+                lastIndex = getLastIndex(file.getAbsolutePath());
+            }
+            else if (System.getenv("FIRMWARE_PATH") != null)// Second check on environment variable
+            {
+                path = System.getenv("FIRMWARE_PATH");
+                file = new File(path);
+                lastIndex = getLastIndex(file.getAbsolutePath());
+            }
+            else
+            {
+                throw new RuntimeException("Firmware binary file path not found !!!");
+            }
+            if (lastIndex > 0)
+            {
+                MetadataGeneratorHelper.generateMetadataJSON(file, lastIndex);
+                System.out.println("Firmware metadata file generated successfully at location :" + file.getAbsolutePath());
+            }
+            else
+            {
+                throw new Exception("Invalid path");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Get last index of a string path;
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    private static int getLastIndex(String path) throws Exception
+    {
+        int lastIndex = -1;
+        String defaultPathSeparator = "/";
+        if (path != null && !path.isEmpty())
+        {
+            String lastChar = path.substring(path.length() - 1, path.length());
+            if (path.contains("\\"))
+            {
+                defaultPathSeparator = "\\";
+            }
+            if (Pattern.matches("[a-z A-Z]", lastChar))
+            {
+                path = path + defaultPathSeparator;
+            }
+            lastIndex = path.lastIndexOf(defaultPathSeparator);
+        }
+        else
+        {
+            throw new Exception("Invalid path, found null or empty value!!!");
+        }
+        return lastIndex;
+    }
 }
--- firmware-packaging-tool/src/com/solarflare/firmware/util/MetadataGeneratorHelper.java
+++ firmware-packaging-tool/src/com/solarflare/firmware/util/MetadataGeneratorHelper.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.solarflare.firmware.model.BinaryFiles;
 import com.solarflare.firmware.model.BootROM;
 import com.solarflare.firmware.model.Controller;
@@ -19,165 +20,221 @@ import com.solarflare.firmware.model.FileHeader;
 import com.solarflare.firmware.model.FirmwareType;
 import com.solarflare.firmware.model.SfFirmware;
 
-public class MetadataGeneratorHelper {
-	static BinaryFiles meta = new BinaryFiles();
-	static Controller controller = new Controller();
-	static BootROM bootRom = new BootROM();
-	static int basePathLastIndex ; 
-	static String FILE_SEPARATOR = File.separator;
-	
-	public static void generateMetadataJSON(File filePath, int lastIndex) throws Exception
-	{
-		basePathLastIndex = lastIndex;
-		searchAndCreateMetae("dat", filePath);
-		Gson gson = new Gson();
-		String json = gson.toJson(meta);
-		writeUsingFileWriter(json, filePath.getPath());
-	}
-	
-	public static void searchAndCreateMetae(String filename, File file)
-	{
-		File[] list = file.listFiles();
-		if (list != null)
-		{
-			for (File fil : list) 
-			{
-				if (fil.isDirectory()) 
-				{
-					searchAndCreateMetae(filename, fil);
-				} 
-				else if (filename.equalsIgnoreCase(getFileExtension(fil))) 
-				{
-					
-					String path = fil.getParent();
-					String relativePath = FILE_SEPARATOR+"firmware"+path.substring(basePathLastIndex, path.length());
-					FileHeader header = getFileHeader(readData(fil, false));
-					if (FirmwareType.FIRMWARE_TYPE_MCFW.ordinal() == header.getType()) 
-					{
-						SfFirmware sfFirmware = new SfFirmware();
-						sfFirmware.setName(fil.getName());
-						sfFirmware.setType(""+header.getType());
-						sfFirmware.setSubtype(""+header.getSubtype());
-						sfFirmware.setVersionString(header.getVersionString());
-						
-						sfFirmware.setPath(relativePath+FILE_SEPARATOR+fil.getName());
-						if(meta != null && meta.getController() != null)
-						{
-							meta.getController().getFiles().add(sfFirmware);
-						}
-						else
-						{
-							List<SfFirmware> sfFirmwareList = new ArrayList<>();
-							sfFirmwareList.add(sfFirmware);
-							controller.setFiles(sfFirmwareList);
-							meta.setController(controller);
-						}
-					} else if (FirmwareType.FIRMWARE_TYPE_BOOTROM.ordinal() == header.getType()) {
-						SfFirmware sfFirmware = new SfFirmware();
-						sfFirmware.setName(fil.getName());
-						sfFirmware.setType(""+header.getType());
-						sfFirmware.setSubtype(""+header.getSubtype());
-						sfFirmware.setVersionString(header.getVersionString());
-						sfFirmware.setPath(relativePath+FILE_SEPARATOR+fil.getName());
-						
-						if(meta != null && meta.getBootROM() != null)
-						{
-							meta.getBootROM().getFiles().add(sfFirmware);
-						}
-						else
-						{
-							List<SfFirmware> sfFirmwareList = new ArrayList<>();
-							sfFirmwareList.add(sfFirmware);
-							bootRom.setFiles(sfFirmwareList);
-							meta.setBootROM(bootRom);
-						}
-					}
-				}
-			}
-		}
-	}
-	private static String getFileExtension(File file) 
-	{
-        String fileName = file.getName();
-        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-        return fileName.substring(fileName.lastIndexOf(".")+1);
-        else return "";
+public class MetadataGeneratorHelper
+{
+    static BinaryFiles metaModel = new BinaryFiles();
+    static Controller controller = new Controller();
+    static BootROM bootROM = new BootROM();
+    static String FILE_SEPARATOR = "/";
+    static int basePathLastIndex;
+    static String METADATA_JSON = "FirmwareMetadata.json";
+    static String BINARY_FILE_EXTENTION = "dat"; 
+
+    /**
+     * Generate JSON file for firmware metadata information
+     * 
+     * @param filePath
+     * @param lastIndex
+     * @throws Exception
+     */
+    public static void generateMetadataJSON(File filePath, int lastIndex) throws Exception
+    {
+        basePathLastIndex = lastIndex;
+        searchBinaryFileAndCreateMetadataJSON(BINARY_FILE_EXTENTION, filePath);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(metaModel);
+        writeJSON(json, filePath.getPath());
     }
-	
-	public static FileHeader getFileHeader(byte[] bytes) {
-		FileHeader header = new FileHeader();
-		ByteBuffer buffer = ByteBuffer.wrap(bytes);
-		buffer.order(ByteOrder.LITTLE_ENDIAN);
-		int ih_magic = buffer.getInt();
-		header.setMagic(ih_magic);
 
-		int ih_version = buffer.getInt();
-		header.setVersion(ih_version);
+    /**
+     * Search a binary file in given path and generate metadata JSON
+     * @param filename
+     * @param file
+     * @throws Exception 
+     */
+    public static void searchBinaryFileAndCreateMetadataJSON(String filename, File file) throws Exception
+    {
+        File[] list = file.listFiles();
+        if (list != null)
+        {
+            for (File fil : list)
+            {
+                if (fil.isDirectory())
+                {
+                    searchBinaryFileAndCreateMetadataJSON(filename, fil);
+                }
+                else if (filename.equalsIgnoreCase(getFileExtension(fil)))
+                {
 
-		int ih_type = buffer.getInt();
-		header.setType(ih_type);
+                    String path = fil.getParent();
+                    String relativePath = FILE_SEPARATOR + "firmware" + FILE_SEPARATOR
+                            + path.substring(basePathLastIndex + 1, path.length());
+                    FileHeader header = getFileHeader(readData(fil, false));
+                    if (FirmwareType.FIRMWARE_TYPE_MCFW.ordinal() == header.getType())
+                    {
+                        // For controller binary file
+                        SfFirmware sfFirmware = new SfFirmware();
+                        sfFirmware.setName(fil.getName());
+                        sfFirmware.setType("" + header.getType());
+                        sfFirmware.setSubtype("" + header.getSubtype());
+                        sfFirmware.setVersionString(header.getVersionString());
 
-		int ih_subtype = buffer.getInt();
-		header.setSubtype(ih_subtype);
+                        sfFirmware.setPath(relativePath + FILE_SEPARATOR + fil.getName());
+                        if (metaModel != null && metaModel.getController() != null)
+                        {
+                            metaModel.getController().getFiles().add(sfFirmware);
+                        }
+                        else
+                        {
+                            List<SfFirmware> sfFirmwareList = new ArrayList<>();
+                            sfFirmwareList.add(sfFirmware);
+                            controller.setFiles(sfFirmwareList);
+                            metaModel.setController(controller);
+                        }
+                    }
+                    else if (FirmwareType.FIRMWARE_TYPE_BOOTROM.ordinal() == header.getType())
+                    {
+                        // For bootrom binary file
+                        SfFirmware sfFirmware = new SfFirmware();
+                        sfFirmware.setName(fil.getName());
+                        sfFirmware.setType("" + header.getType());
+                        sfFirmware.setSubtype("" + header.getSubtype());
+                        sfFirmware.setVersionString(header.getVersionString());
+                        sfFirmware.setPath(relativePath + FILE_SEPARATOR + fil.getName());
 
-		int ih_code_size = buffer.getInt();
-		header.setCodeSize(ih_code_size);
+                        if (metaModel != null && metaModel.getBootROM() != null)
+                        {
+                            metaModel.getBootROM().getFiles().add(sfFirmware);
+                        }
+                        else
+                        {
+                            List<SfFirmware> sfFirmwareList = new ArrayList<>();
+                            sfFirmwareList.add(sfFirmware);
+                            bootROM.setFiles(sfFirmwareList);
+                            metaModel.setBootROM(bootROM);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-		int ih_size = buffer.getInt();
-		header.setSize(ih_size);
+    /**
+     * Get file extension
+     * @param file
+     * @return
+     */
+    private static String getFileExtension(File file) throws Exception
+    {
+        String fileName = file.getName();
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        else
+            return "";
+    }
 
-		int ih_controller_version_min = buffer.getInt();
-		header.setControllerVersionMin(ih_controller_version_min);
+    /**
+     * Get header of a binary file
+     * @param bytes
+     * @return
+     */
+    public static FileHeader getFileHeader(byte[] bytes)throws Exception
+    {
+        FileHeader header = new FileHeader();
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        int ih_magic = buffer.getInt();
+        header.setMagic(ih_magic);
 
-		int ih_controller_version_max = buffer.getInt();
-		header.setControllerVersionMax(ih_controller_version_max);
+        int ih_version = buffer.getInt();
+        header.setVersion(ih_version);
 
-		short ih_code_version_a = buffer.getShort();
-		header.setCodeVersion_a(ih_code_version_a);
+        int ih_type = buffer.getInt();
+        header.setType(ih_type);
 
-		short ih_code_version_b = buffer.getShort();
-		header.setCodeVersion_b(ih_code_version_b);
+        int ih_subtype = buffer.getInt();
+        header.setSubtype(ih_subtype);
 
-		short ih_code_version_c = buffer.getShort();
-		header.setCodeVersion_c(ih_code_version_c);
+        int ih_code_size = buffer.getInt();
+        header.setCodeSize(ih_code_size);
 
-		short ih_code_version_d = buffer.getShort();
-		header.setCodeVersion_d(ih_code_version_d);
-		return header;
-	}
-	
-	public static byte[] readData(File file, boolean isCompleted) {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		InputStream stream  = null;
-		try {
-			byte[] chunk = new byte[1000];
-			int bytesRead;
-			stream = new FileInputStream(file);
-			
-			if (isCompleted) {
-				while ((bytesRead = stream.read(chunk)) > 0) {
-					outputStream.write(chunk, 0, bytesRead);
-				}
-			} else {
-				bytesRead = stream.read(chunk);
-				outputStream.write(chunk, 0, bytesRead);
-			}
-			stream.close();
-		} catch (IOException e) {
-			return null;
-		}
-		return outputStream.toByteArray();
-	}
-	
-	 private static void writeUsingFileWriter(String data, String path) 
-	 {
-	        File file = new File(path+FILE_SEPARATOR+ "FirmwareMetadata.json");
-	        try ( FileWriter fr = new FileWriter(file))
-	        {
-	            fr.write(data);
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+        int ih_size = buffer.getInt();
+        header.setSize(ih_size);
+
+        int ih_controller_version_min = buffer.getInt();
+        header.setControllerVersionMin(ih_controller_version_min);
+
+        int ih_controller_version_max = buffer.getInt();
+        header.setControllerVersionMax(ih_controller_version_max);
+
+        short ih_code_version_a = buffer.getShort();
+        header.setCodeVersion_a(ih_code_version_a);
+
+        short ih_code_version_b = buffer.getShort();
+        header.setCodeVersion_b(ih_code_version_b);
+
+        short ih_code_version_c = buffer.getShort();
+        header.setCodeVersion_c(ih_code_version_c);
+
+        short ih_code_version_d = buffer.getShort();
+        header.setCodeVersion_d(ih_code_version_d);
+        return header;
+    }
+
+    /**
+     * Read chunk data from a file where chunk size is 1000 byte
+     * @param file
+     * @param isCompleted
+     * @return
+     */
+    public static byte[] readData(File file, boolean isCompleted) throws Exception
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        InputStream stream = null;
+        try
+        {
+            byte[] chunk = new byte[1000];
+            int bytesRead;
+            stream = new FileInputStream(file);
+
+            if (isCompleted)
+            {
+                while ((bytesRead = stream.read(chunk)) > 0)
+                {
+                    outputStream.write(chunk, 0, bytesRead);
+                }
+            }
+            else
+            {
+                bytesRead = stream.read(chunk);
+                outputStream.write(chunk, 0, bytesRead);
+            }
+            stream.close();
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * Write string json into a given file 
+     * @param data
+     * @param path
+     */
+    private static void writeJSON(String data, String path) throws Exception
+    {
+
+        File file = new File(path + FILE_SEPARATOR + METADATA_JSON);
+        try (FileWriter fr = new FileWriter(file))
+        {
+            fr.write(data);
+        }
+        catch (IOException e)
+        {
+            throw e;
+        }
+    }
 
 }
