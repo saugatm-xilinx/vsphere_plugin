90bd4ebf05a92b6c41e9b04cce4bf626a0d9fa02
Praveen Kumar <kumar.praveen@msystechnologies.com>
2018-04-26 16:41:33 +0530

    VCPPLUG-295 : change in sign_jar_war.bat script to change way to zip jar and war files
    hg-changeset: eb20d5eb145baad8e1cafa93206b701f47866c55
    hg-branch: p2

--- installer/Windows/WIX/sign_jar_war.bat
+++ installer/Windows/WIX/sign_jar_war.bat
@@ -42,18 +42,25 @@ goto paramError
 	
 :done
 
-for /R "..\..\dist\Tomcat_Server\webapps\solarflare-vcp\" %%f in (*.zip) do (
+set currentDirectory="%CD%"
+set outDirName=..\..\dist\Tomcat_Server\webapps\solarflare-vcp\
+
+for /R "%outDirName%" %%f in (*.zip) do (
 	set dirName=%%~nf
 )
 
-set PathToJARFile=..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\plugins\solarflare-service.jar
-set PathToWARFile=..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\plugins\solarflare-ui.war
+set PathToJARFile=%outDirName%\%dirName%\plugins\solarflare-service.jar
+set PathToWARFile=%outDirName%\%dirName%\plugins\solarflare-ui.war
 
 echo.
 echo - Unzipping %dirName%.zip ...
 echo.
 
-powershell -command "Expand-Archive -Path '..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%.zip' -DestinationPath '..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\'"
+cd %outDirName%
+mkdir %dirName%
+cd %dirName%
+jar -xvf ../%dirName%.zip
+cd %currentDirectory%
 if ERRORLEVEL 1 goto error1
 
 goto end1
@@ -72,13 +79,13 @@ echo.
 echo - Signing JAR and WAR files...
 echo.
 
-keytool -list -storetype pkcs12 -keystore "%PathToCACerticate%" -storepass %CertificatePassword%
+keytool -list -keystore "%PathToCACerticate%" -storepass %CertificatePassword%
 if ERRORLEVEL 1 goto error2
 
-jarsigner -storetype pkcs12 -tsa %TimeStampURL% -keystore "%PathToCACerticate%" -storepass %CertificatePassword% "%PathToJARFile%" %AliasName%
+jarsigner -keystore "%PathToCACerticate%" -tsa %TimeStampURL% -storepass %CertificatePassword% "%PathToJARFile%" %AliasName%
 if ERRORLEVEL 1 goto error2
 
-jarsigner -storetype pkcs12 -tsa %TimeStampURL% -keystore "%PathToCACerticate%" -storepass %CertificatePassword% "%PathToWARFile%" %AliasName%
+jarsigner -keystore "%PathToCACerticate%" -tsa %TimeStampURL% -storepass %CertificatePassword% "%PathToWARFile%" %AliasName%
 if ERRORLEVEL 1 goto error2
 
 goto end2
@@ -87,14 +94,14 @@ goto end2
 echo.
 echo - Sign jar and war files failed!
 echo - Make sure all agruments are passing correctly.
-rmdir /s /q "..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\"
+rmdir /s /q "%outDirName%\%dirName%\"
 exit /B 1
 
 :end2
 echo.
 :: delete zip file after unzipping successfully
-if EXIST "..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%.zip" ( 
-del /s /q "..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%.zip" 
+if EXIST "%outDirName%\%dirName%.zip" ( 
+del /s /q "%outDirName%\%dirName%.zip" 
 ) ELSE ( 
 echo "ZIP file not exist" 
 )
@@ -105,10 +112,10 @@ echo.
 echo - Zipping %dirName% ...
 echo.
 
-powershell -command "Compress-Archive -Path '..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\*' -DestinationPath '..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%.zip'"
+jar -cvMf %outDirName%\%dirName%.zip -C %outDirName%\%dirName%\ .
 if ERRORLEVEL 1 goto error3
 
-rmdir /s /q "..\..\dist\Tomcat_Server\webapps\solarflare-vcp\%dirName%\"
+rmdir /s /q "%outDirName%\%dirName%\"
 if ERRORLEVEL 1 goto error3
 
 goto end3
