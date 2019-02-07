0915237454172be30681b4de916890e55a5fa881
saugat mitra smitra@solarflare.com <unknown>
2019-02-07 08:11:16 +0000

    Bug 83757: Changes made to publish zip file containing Jar/War files on ivy
    hg-changeset: 5edc75fd77eae894da0a82d8a9e639892237966f
    hg-branch: new_framework

---
+++ installer/Scripts/publish_to_ivy.py
@@ -0,0 +1,75 @@
+"""
+This script publishes the file on ivy according to the
+command line inputs provided.
+"""
+
+import os
+import sys
+import shutil
+import optparse
+import xml.etree.ElementTree as xmlparser
+
+def fail(msg):
+    """ Function prints the error message and exits """
+    print(msg)
+    sys.exit(1)
+
+def main():
+    """ Starting point of script execution. Parses the inputs
+        calls functions to copy images and generate json file. """
+    try:
+        parser = optparse.OptionParser(usage=
+                                       """Usage: %prog [options]
+                                          """,
+                                       version="%prog 1.0")
+        parser.add_option("-r", "--revision", dest='rev_tag',
+                          help="revision to write files in")
+        parser.add_option("-n", "--name", dest='file_name',
+                          help="msi file name to publish")
+
+        options, args = parser.parse_args()
+        if ((len(args) > 1) or
+            (options.rev_tag is None)):
+            parser.print_help()
+            fail("Exiting")
+        if not options.rev_tag.startswith('v'):
+            options.rev_tag = 'v' + options.rev_tag
+        ret_val = os.system('hg clone /project/hg/incoming/ivybase')
+        if ret_val != 0:
+            fail("Not able to clone ivybase")
+        curr_dir = os.getcwd()
+        ivy_dir = curr_dir + '/ivybase'
+        ivy_xml_file = curr_dir + '/ivy.xml'
+        tree = xmlparser.parse(ivy_xml_file)
+        root_node = tree.getroot()
+        if options.file_name is None:
+            options.file_name = 'installer.zip'
+        lhs,rhs = options.file_name.split(".")
+        if ((rhs == 'zip') or
+            (rhs == 'msi')):
+            for child in root_node:
+                if child.tag == "info":
+                    child.set('revision',options.rev_tag)
+                if ((child.tag == "publications") and
+                    (rhs == 'msi')):
+                    for artifact in child.getchildren():
+                        artifact.set('name',lhs)
+                        artifact.set('ext','msi')
+                        artifact.set('type','bin')
+            tree.write(ivy_xml_file)
+            ret_val = os.system(ivy_dir + '/scripts/ivy_publish ivy.xml')
+            if ret_val != 0:
+                fail("Not able to publish on ivy")
+        ret_val = os.system('rm -rf ivybase')
+        if ret_val != 0:
+            fail("Not able to remove ivybase")
+    except KeyError:
+        fail("Image variant not determined. Exiting.")
+    except OSError:
+        fail("OS Environment error. Exiting.")
+    except IOError:
+        fail("Publishing to ivy failed. Exiting.")
+
+if __name__ == '__main__':
+    main()
+
--- solarflare-ui/tools/build-plugin-package.bat
+++ solarflare-ui/tools/build-plugin-package.bat
@@ -3,6 +3,20 @@ REM --- Windows script
 REM --- (if Ant runs out of memory try defining ANT_OPTS=-Xmx512M)
 
 @setlocal
+@set argC=0
+@set help = "help"
+@set size=0
+for %%x in (%*) do Set /A argC+=1
+IF %argC% == 0 echo No arguments provided...do build-plugin-package.bat help
+
+@IF %1% == help (
+    echo "For publishing files on ivy ..."
+    echo build-plugin-package.bat publish username password remote_machine_name fw_family revison_tag
+    echo "For Building the package only ..."
+    echo build-plugin-package.bat package
+    goto end
+)
+
 @IF not defined ANT_HOME (
    @echo BUILD FAILED: You must set the env variable ANT_HOME to your Apache Ant folder
    goto end
@@ -20,7 +34,27 @@ REM --- (if Ant runs out of memory try defining ANT_OPTS=-Xmx512M)
    @echo %VSPHERE_SDK_HOME%\libs\vsphere-client-lib.swc is missing
    goto end
 )
+    @echo "Building package files ......."
+    @call "%ANT_HOME%\bin\ant" -f %~dp0\build-plugin-package.xml
 
-@call "%ANT_HOME%\bin\ant" -f %~dp0\build-plugin-package.xml
+@set publish="publish"
+@IF %1% == publish (
+   @echo "Publishing files on ivy........."
+   wget -q http://source.uk.solarflarecom.com/hg/incoming/esxi_sfc/rawfile/firmware_vib/fetch_image.c
+   wget -q http://source.uk.solarflarecom.com/hg/incoming/esxi_sfc/rawfile/firmware_vib/fetch_image.h
+   wget -q http://source.uk.solarflarecom.com/hg/incoming/esxi_sfc/rawfile/firmware_vib/fetch_firmware_image.py
+   gcc -shared fetch_image.h -o libfetch_image.dll fetch_image.c
+   python fetch_firmware_image.py ../../installer/dist/Tomcat_Server/webapps/ %5 %2 %4
+   plink -ssh -l %2 -pw %3 chisel.uk.solarflarecom.com "mkdir firmware_publish;chmod -R 777 firmware_publish"
+   @echo Please wait ...it's going to take some time....
+   pscp -q -r -l %2 -pw %3 ../../installer chisel.uk.solarflarecom.com:/home/%2%/firmware_publish
+   plink -ssh -l %2 -pw %3 chisel.uk.solarflarecom.com "cd firmware_publish; zip -r installer.zip installer/; mv installer.zip installer/Scripts/"
+   plink -ssh -l %2 -pw %3 chisel.uk.solarflarecom.com "cd firmware_publish/installer/Scripts/; python publish_to_ivy.py -r %6; cd ../../../; rm -rf firmware_publish"
+
+   del fetch_image.c
+   del fetch_image.h
+   del fetch_firmware_image.py
+   del libfetch_image.dll
+)
 
 :end
