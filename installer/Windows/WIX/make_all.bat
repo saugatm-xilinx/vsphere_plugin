c29aeb90e5451e55b90f363edc23944652151822
saugat mitra smitra@solarflare.com <unknown>
2019-06-11 08:17:22 +0100

    bug81528: Updating version number to 1.2.0.1010
    hg-changeset: 329302ff03b5f97f98c7ca75e62fdba8aa8f3635
    hg-branch: new_framework

--- installer/Windows/WIX/make_all.bat
+++ installer/Windows/WIX/make_all.bat
@@ -9,7 +9,7 @@ set TimeStampURL="http://timestamp.globalsign.com/scripts/timestamp.dll"
 set LabelId="836E39711EBF5F40"
 set SignToolPath="C:\Program Files (x86)\Windows Kits\8.0\bin\x64"
 :: Variable ProductVersion is set by scripts/createtag script
-set ProductVersion=1.2.0.1009
+set ProductVersion=1.2.0.1010
 set SHA1Fingerprint=E9A92BF58F1C669E09B53CE2CD00521FDE1404AF
 set JDKPATH="C:\Program Files\Java\jdk1.8.0_162\bin"
 ) else if "%buildType%" equ "release" (
@@ -18,7 +18,7 @@ set TimeStampURL="http://timestamp.globalsign.com/scripts/timestamp.dll"
 set LabelId="te-d933afc6-2922-4ac6-9226-426035a34c17"
 set SignToolPath="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\"
 :: Variable ProductVersion is set by scripts/createtag script
-set ProductVersion=1.2.0.1009
+set ProductVersion=1.2.0.1010
 set SHA1Fingerprint=CB42DC129745B195C0E27DC3BB26E45208A1B470
 set JDKPATH="C:\Program Files\Java\jdk1.8.0_172\bin"
 ) else (
